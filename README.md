d7c461697735cf3dac56b667c350e067f2b22d34
cauchy221 <1321378776@qq.com>
2024-10-05 18:20:56 -0400

    add first 5M tokens of training data

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Lazy Pretrain
+
+## Extract first 5M tokens from the training data
+1. Decompress the first subset of the training data
+```bash
+pv data/the_pile/train/00.jsonl.zst | zstd -d > data/train_00/00.jsonl
+```
+2. (Optional) Check the first 5 lines and keys of each line
+```bash
+head -n 5 data/train_00/00.jsonl
+head -n 1 data/train_00/00.jsonl | jq 'keys'
+```
+3. Extract the first 5M tokens (2723 lines). The result will be saved in `data/train_00/00_5m.jsonl`
+```bash
+python extract_subset.py
+```
---
+++ src/extract_subset.py
@@ -0,0 +1,38 @@
+import json
+from transformers import AutoTokenizer
+from tqdm import tqdm
+
+def extract_tokens(input_file, output_file, target_token_count):
+    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-70m")
+    
+    total_tokens = 0
+    extracted_data = []
+
+    # Open the input file and read line by line
+    with open(input_file, 'r') as infile:
+        for line in tqdm(infile, desc="Processing lines"):
+            data = json.loads(line)
+            text = data['text']
+            
+            tokens = tokenizer.encode(text)
+            token_count = len(tokens)
+            total_tokens += token_count
+            extracted_data.append(data)
+            
+            if total_tokens >= target_token_count:
+                break
+    
+    print(f"Extracted {total_tokens} tokens")
+    
+    with open(output_file, 'w') as outfile:
+        for item in extracted_data:
+            json.dump(item, outfile)
+            outfile.write('\n')
+
+if __name__ == "__main__":
+    input_file = "data/train_00/00.jsonl"
+    output_file = "data/train_00/00_5m.jsonl"
+    target_tokens = 5_000_000
+    
+    extract_tokens(input_file, output_file, target_tokens)
+    print(f"Extracted data written to {output_file}")
