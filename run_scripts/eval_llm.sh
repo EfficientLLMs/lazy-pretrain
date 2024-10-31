6c1ab36b42fdf8bd2c518c450f0ca18c127940f9
vibhamasti <vsmasti@gmail.com>
2024-10-31 14:03:00 -0400

    finished more lora exp

--- run_scripts/eval_llm.sh
+++ run_scripts/eval_llm.sh
@@ -1,49 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=eval_freeze_70m_410m_tokens_5m
+#SBATCH --job-name=eval_freeze_70m_step140000_410m_tokens_1b_alpha256-allmod
 #SBATCH --mem=32G
-#SBATCH --gres=gpu:2080Ti:2
-#SBATCH --output=.slurm_logs/eval_freeze_70m_410m_tokens_5m.out
+#SBATCH --gres=gpu:A6000:2
+#SBATCH --output=.slurm_logs/eval_freeze_70m_step140000_410m_tokens_1b_alpha256-allmod.out
 #SBATCH --time=01-00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=vmasti@andrew.cmu.edu
 
 
-# python src/eval_llm.py \
-#     --base_model_path "models/pythia-410m-to-pythia-1.4b" \
-#     --lora_path "models/pythia-410m-to-pythia-1.4b-lora-5m" \
-#     --tokenizer_path "EleutherAI/pythia-410m" \
-#     --eval_results_path "eval/r256_410m_1.4b_eval_results" \
-#     --parallelize \
-#     --tasks "lambada_openai" "arc_easy" "arc_challenge" "hellaswag" "piqa" "winogrande" "sciq" "logiqa" "logiqa2" "openbookqa" \
-
-
-# python src/eval_llm.py \
-#     --base_model_path "EleutherAI/pythia-410m" \
-#     --tokenizer_path "EleutherAI/pythia-410m" \
-#     --eval_results_path "eval/410m_raw_results_openai" \
-#     --parallelize \
-#     --tasks "lambada_openai"
-
-# python src/eval_llm.py \
-#     --base_model_path "EleutherAI/pythia-70m" \
-#     --tokenizer_path "EleutherAI/pythia-70m" \
-#     --eval_results_path "eval/70m_raw_results_openai" \
-#     --parallelize \
-#     --tasks "lambada_openai"
-
-# python src/eval_llm.py \
-#     --base_model_path "models/pythia-410m-to-pythia-1.4b" \
-#     --tokenizer_path "EleutherAI/pythia-410m" \
-#     --eval_results_path "eval/r256_410m_1.4b_eval_raw_results_openai" \
-#     --parallelize \
-#     --tasks "lambada_openai"
-    
-    # "arc_easy" "arc_challenge" "hellaswag" "piqa" "winogrande" "sciq" "logiqa" "logiqa2" "openbookqa" \
 
 python src/eval_llm.py \
-    --base_model_path "models/pythia-70m-to-pythia-410m-freeze-5m" \
+    --base_model_path "models/pythia-70m-step140000-to-pythia-410m" \
+    --lora_path "models/pythia-70m-step140000-to-pythia-410m-lora-alpha256-allmod" \
     --tokenizer_path "EleutherAI/pythia-70m" \
-    --eval_results_path "eval/freeze_70m_410m_eval_results_5m" \
+    --eval_results_path "eval/lora_70m_step140000_410m_eval_results_1b_alpha256-allmod" \
     --tasks "lambada_openai" "arc_easy" "arc_challenge" "hellaswag" "piqa" "winogrande" "sciq" "logiqa" "logiqa2" "openbookqa"
 
 
--- run_scripts/pretrain_lora.sh
+++ run_scripts/pretrain_lora.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=100m_lora_r256_70m_410m
+#SBATCH --job-name=100m_lora_r256_70m_410m_alpha256-allmod
 #SBATCH --mem=32G
-#SBATCH --gres=gpu:2080Ti:3
-#SBATCH --output=.slurm_logs/100m_r256_70m_410m_lora.out
+#SBATCH --gres=gpu:A6000:8
+#SBATCH --output=.slurm_logs/1b_r256_70m_410m_lora_alpha256-allmod.out
 #SBATCH --time=01-00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=vmasti@andrew.cmu.edu
 
 
-accelerate launch src/pretrain_lora.py \
-    --grown_model "models/pythia-70m-to-pythia-410m" \
-    --tokenizer "EleutherAI/pythia-70m" \
-    --dataset "data/train_00/00_100m.pt" \
-    --seed 1234 \
-    --rank 256 \
-    --batch_size 8 \
-    --lr 1e-4 \
-    --output_dir "models/pythia-70m-to-pythia-410m-lora-100m"
-
-
-# accelerate launch src/pretrain_lora.py \
-#     --grown_model "models/pythia-70m-to-pythia-410m" \
-#     --tokenizer "EleutherAI/pythia-70m" \
-#     --dataset "data/train_00/00_100m.pt" \
-#     --seed 1234 \
-#     --rank 256 \
-#     --batch_size 8 \
-#     --lr 1e-4 \
-#     --output_dir "models/pythia-70m-to-pythia-410m-lora-20m"
-
-
-
 # accelerate launch src/pretrain_lora.py \
 #     --grown_model "models/pythia-410m-to-pythia-1.4b" \
 #     --tokenizer "EleutherAI/pythia-410m" \
@@ -39,32 +16,39 @@ accelerate launch src/pretrain_lora.py \
 #     --rank 256 \
 #     --batch_size 8 \
 #     --lr 1e-4 \
-#     --output_dir "models/pythia-70m-to-pythia-410m-lora"
+#     --output_dir "models/pythia-70m-to-pythia-410m-lora-"
 
 
-
-# accelerate launch src/pretrain_lora.py \
-#     --grown_model "models/pythia-410m-to-pythia-1.4b" \
-#     --tokenizer "EleutherAI/pythia-410m" \
-#     --dataset "data/train_00/00_20m.pt" \
-#     --seed 1234 \
-#     --rank 256 \
-#     --batch_size 16 \
-#     --lr 1e-4 \
-#     --output_dir "models/pythia-410m-to-pythia-1.4b-lora"
+# --grown_model "models/pythia-410m-to-pythia-1.4b" \
+# --dataset "data/train_00/00_20m.pt" \
 
 accelerate launch src/pretrain_lora.py \
-    --grown_model "models-xinyue/pythia-70m-step140000-to-pythia-410m" \
-    --tokenizer "EleutherAI/pythia-70m" \
+    --grown_model "models/pythia-70m-step140000-to-pythia-410m" \
+    --tokenizer "EleutherAI/pythia-410m" \
     --seed 1234 \
     --rank 256 \
-    --batch_size 32 \
-    --lr 1e-4 \
-    --output_dir "models-xinyue/pythia-70m-step140000-to-pythia-410m-lora" \
+    --lora_alpha 256 \
+    --batch_size 64 \
+    --lr 1e-5 \
+    --output_dir "models/pythia-70m-step140000-to-pythia-410m-lora-alpha256-allmod" \
     --use_on_the_fly \
     --first_idx 19 \
     --last_idx 20 \
-    --num_tokens 1000000000 \
+    --num_tokens 1_000_000_000 \
     --chunk_size 512
+
+# accelerate launch src/pretrain_lora.py \
+#     --grown_model "models-xinyue/pythia-70m-step140000-to-pythia-410m" \
+#     --tokenizer "EleutherAI/pythia-70m" \
+#     --seed 1234 \
+#     --rank 256 \
+#     --batch_size 32 \
+#     --lr 1e-4 \
+#     --output_dir "models-xinyue/pythia-70m-step140000-to-pythia-410m-lora" \
+#     --use_on_the_fly \
+#     --first_idx 19 \
+#     --last_idx 20 \
+#     --num_tokens 1000000000 \
+#     --chunk_size 512
 
     
