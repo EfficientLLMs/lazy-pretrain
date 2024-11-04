8f33770e819be813628424975c6b277e8fce84bb
Vibha Masti <vsmasti@gmail.com>
2024-11-04 16:20:20 -0500

    Changed checkpoint to be 142k

--- run_scripts/run_grow.sh
+++ run_scripts/run_grow.sh
@@ -22,7 +22,7 @@ python src/grow/grow.py \
     --large_depth 24 \
     --large_width 1024 \
     --depth_growth "alternate" \
-    --output_dir "models/pythia-70m-step140000-to-pythia-410m" \
+    --output_dir "models/pythia-70m-step142000-to-pythia-410m" \
     --checkpoint_step "step142000"
     
 # "step3000",
