679684b0c19247fc33b58102d6cf8afd60d33c58
cauchy221 <1321378776@qq.com>
2024-11-18 16:49:07 -0500

    fix relora

--- run_scripts/pretrain_relora.sh
+++ run_scripts/pretrain_relora.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-#SBATCH --job-name=pythia-70m-step142000-2b-relora
+#SBATCH --job-name=pythia-70m-step141000-4b-relora-new
 #SBATCH --mem=80G
 #SBATCH --gres=gpu:A6000:8
+#SBATCH --exclude=babel-1-23
 #SBATCH --partition=general
-#SBATCH --output=.slurm_logs/pythia-70m-step142000-2b-relora.out
+#SBATCH --output=.slurm_logs/pythia-70m-step141000-4b-relora-new.out
 #SBATCH --time=02-00:00
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=vmasti@andrew.cmu.edu
-#SBATCH --exclude=shire-1-6,shire-1-10,babel-1-27,babel-1-23
+#SBATCH --mail-user=xinyuel4@andrew.cmu.edu
 
-# export NCCL_P2P_DISABLE=1
-
-accelerate launch src/pretrain/pretrain_relora.py \
-    --grown_model "models/pythia-70m-step142000-to-pythia-410m" \
+python src/pretrain/pretrain_relora.py \
+    --grown_model "models-xinyue/pythia-70m-step141000-to-pythia-410m" \
     --tokenizer "EleutherAI/pythia-410m" \
     --seed 1234 \
     --rank 256 \
     --lora_alpha 256 \
     --batch_size 64 \
     --lr 1e-5 \
-    --output_dir "models/pythia-70m-step142000-2b-relora" \
+    --output_dir "models-xinyue/pythia-70m-step141000-4b-relora-new" \
     --use_on_the_fly \
     --first_idx 19 \
     --last_idx 20 \
-    --num_tokens 2_000_000_000 \
+    --num_tokens 4_000_000_000 \
     --chunk_size 512 \
     --scheduler "cosine_restarts" \
-    --min_lr_ratio 0.1
+    --min_lr_ratio 0.1 \
+    --wandb_entity "irisiris" \
+    --do_extact_lora
     # we don't need others; but we need to specify the num_restarts in the main code
     # --relora_steps 350 \
     # --cycle_length 350 \
--- src/pretrain/pretrain_relora.py
+++ src/pretrain/pretrain_relora.py
@@ -10,11 +10,18 @@ import wandb
 from tqdm import tqdm
 from torch.nn.utils import clip_grad_norm_
 from torch.utils.data import Dataset, DataLoader
+import sys
 
 # Relative imports
 from utils import CustomBinFileDataset, seed_all, train
 from relora import ReLoRaModel
 from relora_utils import get_scheculer, train_relora
+
+
+def count_parameters(model):
+    total_params = sum(p.numel() for p in model.parameters())
+    trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return total_params, trainable_params
 
 
 def parse_args():
@@ -69,6 +76,8 @@ def parse_args():
     parser.add_argument('--warmup_steps', type=int, default=150)
     parser.add_argument('--restart_warmup_steps', type=int, default=150)  # same as the intial warmup
     parser.add_argument('--min_lr_ratio', type=float, default=0.1)  # decay to 0.1 * lr
+    parser.add_argument('--do_extact_lora', action='store_true',
+                        help='Whether to train only the lora_A and lora_B')
     
     args = parser.parse_args()
     return args
@@ -114,7 +123,7 @@ def main():
 
     # This should be specified based on the total number of tokens
     # For 1B, I use 5 restarts
-    num_restarts = 5
+    num_restarts = 20
     desired_cycle_length = steps_per_gpu // num_restarts
     cycle_length = desired_cycle_length * num_gpus
     
@@ -164,7 +173,12 @@ def main():
     # Use ReLoRA
     model = ReLoRaModel(
         model,
-        target_modules="all-linear",
+        target_modules=[
+            "query_key_value",
+            "dense",
+            "dense_h_to_4h",
+            "dense_4h_to_h",
+        ],
         r=args.rank,
         lora_alpha=args.lora_alpha,
         lora_dropout=0.05,
@@ -175,6 +189,25 @@ def main():
         lora_only=False,
         trainable_scaling=False,
     )
+
+    # Note: the default relora config set everything to be trainable, except it sets the dense weight to be lora_A and lora_B
+    if args.do_extact_lora:
+        # Set all parameters to be non-trainable
+        # then set only the lora_A and lora_B 
+        for name, param in model.named_parameters():
+            param.requires_grad = False
+            if "lora_A" in name or "lora_B" in name:
+                param.requires_grad = True
+
+    # print trainable parameters
+    total_params, trainable_params = count_parameters(model)
+    print(f"Total trainable parameters: {trainable_params}")
+    print(f"Train ratio: {trainable_params / total_params:.4f}")
+
+    for name, param in model.named_parameters():
+        print(f"{name}: requires_grad = {param.requires_grad}, shape = {param.shape}")
+
+    sys.exit()
 
     # Optimizer
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
